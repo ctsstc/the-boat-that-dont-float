ef2ad1d898ac8ed0282cad038edf51050b2c6be7
Cody Swartz <ctsstc@gmail.com>
2017-09-01 12:58:08 -0700

    Round 2

--- main.py
+++ main.py
@@ -23,7 +23,36 @@ sml.feature.outliers('SibSp', upper=99)
 sml.feature.density('Age')
 sml.train[['Age', 'Age_density']].head()
 
+sml.feature.density('Ticket')
+sml.train[['Ticket', 'Ticket_density']].head()
+
 # Shows to be low density, let's scrap this!
 sml.feature.drop(['Ticket'])
 
+sml.plot.crosstab('Survived', 'SibSp')
 
+sml.plot.crosstab('Survived', 'Parch')
+
+# Add some new features, and transform data
+sml.feature.fillna(a='Cabin', new='Z')
+sml.feature.extract(new='Deck', a='Cabin', regex='([A-Z]){1}')
+sml.feature.drop(['Cabin'])
+sml.feature.mapping('Sex', {'male': 0, 'female': 1})
+sml.feature.sum(new='FamilySize', a='Parch', b='SibSp')
+sml.feature.add('FamilySize', 1)
+
+sml.plot.bar('FamilySize', 'Survived')
+sml.plot.bar('Deck', 'Survived')
+# Drop old fields now that we have new ones
+sml.feature.drop(['Parch', 'SibSp'])
+
+# Fill in missing values with median values
+# ie: There are plenty of Cabin/Deck values missing from the data
+sml.feature.impute()
+
+sml.info()
+sml.plot.importance()
+sml.train.head()
+
+sml.feature.extract(new='Title', a='Name', regex=' ([A-Za-z]+)\.')
+sml.plot.crosstab('Title', 'Sex')
