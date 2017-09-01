3d6808ae2fef1b5b984916045b716c63dc12c313
Cody Swartz <ctsstc@gmail.com>
2017-09-01 15:09:49 -0700

    The End

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ sml.feature.impute()
 #sml.plot.importance()
 sml.train.head()
 
-sml.feature.extract(new='Title', a='Name', regex=' ([A-Za-z]+)\.')
-sml.plot.crosstab('Title', 'Sex')
+sml.feature.extract(new='Title', a='Name', regex=r' ([A-Za-z]+)\.')
+#sml.plot.crosstab('Title', 'Sex')
 
 # Static Data Re-Mapping
 sml.feature.replace('Title', 'Mlle', 'Miss')
@@ -67,3 +67,71 @@ sml.feature.replace(a='Title', match=['Lady', 'Countess','Capt', 'Col',\
 
 sml.train[['Name', 'Title']].head()
 
+sml.feature.drop(['Name'])
+sml.feature.labels(['Title', 'Embarked', 'Deck'])
+sml.train.head()
+
+#sml.plot.importance()
+#sml.plot.correlate()
+#sml.plot.distribute()
+
+sml.eda()
+sml.model.data()
+
+
+# Tuning
+
+select_params = {'max_depth': [3,5,7], 'min_child_weight': [1,3,5]}
+fixed_params = {'learning_rate': 0.1, 'subsample': 0.8, 
+                'colsample_bytree': 0.8, 'seed':0, 
+                'objective': 'binary:logistic'}
+
+sml.xgb.hyper(select_params, fixed_params)
+
+
+select_params = {'learning_rate': [0.3, 0.1, 0.01], 'subsample': [0.7,0.8,0.9]}
+fixed_params = {'max_depth': 3, 'min_child_weight': 1, 
+                'colsample_bytree': 0.8, 'seed':0, 
+                'objective': 'binary:logistic'}
+
+sml.xgb.hyper(select_params, fixed_params)
+
+tuned_params = {'learning_rate': 0.1, 'subsample': 0.8, 
+                'max_depth': 3, 'min_child_weight': 1,
+                'seed':0, 'colsample_bytree': 0.8, 
+                'objective': 'binary:logistic'}
+sml.xgb.cv(tuned_params)
+
+# sml.xgb.cv_results.tail(5)
+
+
+tuned_params['n_estimators'] = sml.xgb.cv_results.shape[0] - 1
+sml.xgb.params(tuned_params)
+
+
+# Evaluation & Prediction
+
+sml.xgb.classifier()
+
+sml.model.evaluate()
+sml.plot.model_ranks()
+
+sml.model.ranks()
+
+
+# Predict and Ft. Select
+
+sml.xgb.fit()
+sml.xgb.predict()
+
+sml.plot.xgb_importance()
+
+sml.xgb.feature_selection()
+
+sml.xgb.sample_accuracy()
+
+sml.save_results(
+    columns={ 'PassengerId': sml.uid,
+             'Survived': sml.xgb.predictions }, 
+    file_path='output/titanic-speedml-{}.csv'.format(sml.slug()))
+sml.slug()
